91d3b81270c818a89032a290937b4a8293cbb892
Pavlo Kolesnyk <56836859+kolespaul@users.noreply.github.com>
2021-09-09 15:43:43 +0300

    Add files via upload

---
+++ homework3/app.js
@@ -0,0 +1,34 @@
+// Task #1
+
+Array.prototype.myFilter = function(callback, thisUnnArg) {
+
+    let res = [];
+
+    for (let i = 0; i < this.length; i++) {
+        if (callback.call(thisUnnArg, this[i], i, this)) {
+            res.push(this[i]);
+        }
+    }
+
+    return res;
+};
+
+// Task #2
+
+
+function createDebounceFunction(f, ms) {
+    let timeout;
+
+    return function() {
+        const fnCall = () => {f.apply(this, arguments)}
+
+        clearTimeout(timeout);
+
+        timeout = setTimeout(fnCall, ms);
+    };
+  
+}
+
+
+
+
